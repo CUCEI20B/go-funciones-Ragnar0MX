ea2ec7f27752a4c9d77a6780de1bef93f1cc657e
Moises Rafael <rafael.plascencia@alumnos.udg.mx>
2020-10-21 10:55:08 -0500

    prueba01

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,28 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(s []int64) {
+	acept := true
+	var aux int64 = 0
+	for i, val := range s {
+		if aux == 0 {
+			aux = val
+		}
+		if aux != -100 {
+			if aux > val {
+				s[i-1] = s[i]
+				s[i] = aux
+				acept = false
+			}
+			if aux <= s[i] {
+				aux = s[i]
+			}
+		}
+	}
+	if !acept {
+		Burbuja(s)
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
